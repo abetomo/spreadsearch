80ba41a32f4b8e2d004d1ef17804d34f747fddd1
abetomo <abe@enzou.tokyo>
2017-10-11 11:12:19 +0900

    Add methods to parse arguments

--- bin/spreadsearch.js
+++ bin/spreadsearch.js
@@ -6,37 +6,7 @@ const ss = new (require('..'))()
 const usage = `spreadsearch [NAME] init|clean|update|console
 spreadsearch [NAME] search QUERY`
 
-const [name, type, query] = (() => {
-  switch (process.argv.length) {
-    case 3:
-      switch (process.argv[2]) {
-        case 'init':
-        case 'clean':
-        case 'update':
-        case 'console':
-        case 'help':
-          return ['default', process.argv[2]]
-      }
-      return [process.argv[2]]
-    case 4:
-      switch (process.argv[3]) {
-        case 'init':
-        case 'clean':
-        case 'update':
-        case 'console':
-        case 'help':
-          return [process.argv[2], process.argv[3]]
-      }
-      if (process.argv[2] == 'search') {
-        return ['default', process.argv[2], process.argv[3]]
-      }
-    case 5:
-      if (process.argv[3] == 'search') {
-        return [process.argv[2], process.argv[3], process.argv[4]]
-      }
-  }
-  return ['default', 'console']
-})()
+const [name, type, query] = ss.parseArgs(process.argv)
 
 if (type === 'help') {
   console.log(usage)
